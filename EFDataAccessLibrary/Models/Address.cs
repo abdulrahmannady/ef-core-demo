8a82a7a90c450fd32d0308d48863b9cf5c7889cd
abdulrahmannady <abdulrahmann@silverkeytech.com>
2022-02-26 13:29:02 +0200

    change column size and make it not nullable using data annotation

--- EFDataAccessLibrary/Models/Address.cs
+++ EFDataAccessLibrary/Models/Address.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFDataAccessLibrary.Models
 {
     public class Address
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
         public string StreetAddress { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string City { get; set; }
+
+        [Required]
+        [MaxLength(10)]
         public string ZipCode { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Column(TypeName = "varchar(10)")] // using explicit datatype as we don't want uni-coded zipcode it use only ascii
         public string State { get; set; }
     }
 }
