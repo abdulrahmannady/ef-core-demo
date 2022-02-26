903ef1969cc2603c03171268f92e185c148c2498
abdulrahmannady <abdulrahmann@silverkeytech.com>
2022-02-26 15:33:34 +0200

    batch insert

--- EFDemoWeb/Pages/Index.cshtml.cs
+++ EFDemoWeb/Pages/Index.cshtml.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace EFDemoWeb.Pages
@@ -11,15 +14,31 @@ namespace EFDemoWeb.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly PeopleContext _db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, PeopleContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public void OnGet()
         {
+            LoadSampleData();
+        }
 
+        private void LoadSampleData()
+        {
+            if(_db.People.Count() == 0)
+            {
+                //Load temp data
+                string file = System.IO.File.ReadAllText("generated.json");
+                // deserlize it into array of people model
+                var people = JsonSerializer.Deserialize<List<Person>>(file);
+                // insert query to add people
+                _db.AddRange(people);
+                _db.SaveChanges();
+            }
         }
     }
 }
